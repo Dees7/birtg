5be57441ffdccc2306f3460334e9a385d151cd5c
Denis Taranushin <tda@chelinvest.ru>
2024-11-30 22:35:14 +0100

    Get settings from file

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Birthday bot
+
+## Settings 
+Stored in setting.json
+
+```
+{
+    "token": "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11",
+    "chat_id": -1
+}
+```
--- main.go
+++ main.go
@@ -1,22 +1,60 @@
 ﻿package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
+	"os"
 	"time"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
+type Settings struct {
+	Token  string `json:"token"`
+	ChatID int64  `json:"chat_id"`
+}
+
 func main() {
+	// Загружаем настройки
+	var settings Settings
+	//var people []Person
+
+	// Открываем JSON-файл
+	jsonFile, err := os.Open("setting.json")
+	if err != nil {
+		log.Fatalf("Не удалось открыть файл: %s", err)
+	}
+	defer jsonFile.Close()
+
+	// Получаем информацию о файле
+	fileInfo, err := jsonFile.Stat()
+	if err != nil {
+		log.Fatalf("Ошибка при получении информации о файле: %s", err)
+	}
+
+	// Создаем срез байтов для хранения содержимого файла
+	byteValue := make([]byte, fileInfo.Size())
+
+	// Читаем содержимое файла
+	_, err = jsonFile.Read(byteValue)
+	if err != nil {
+		log.Fatalf("Ошибка при чтении файла: %s", err)
+	}
+
+	// Парсим JSON данные
+	err = json.Unmarshal(byteValue, &settings)
+	if err != nil {
+		log.Fatalf("Ошибка при парсинге JSON: %s", err)
+	}
 	// Создаем нового бота, используя токен, который вы получили от BotFather
-	bot, err := tgbotapi.NewBotAPI("token")
+	bot, err := tgbotapi.NewBotAPI(settings.Token)
 	if err != nil {
 		log.Panic(err)
 	}
 
 	// ID чата, в который будет отправляться сообщение
-	chatID := int64(-1) // Замените на ID вашего чата
+	chatID := int64(settings.ChatID) // Замените на ID вашего чата
 
 	// Запускаем задачу, которая будет отправлять сообщение каждый день в определенное время
 	scheduleMessage(bot, chatID)
